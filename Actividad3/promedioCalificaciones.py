cd1c1e800f11fdd987697ebbfb3a1867c830ca98
ClaudiaLiraB <clucialirab@uamv.edu.ni>
2025-04-22 07:29:50 -0600

    first commit

---
+++ Actividad3/promedioCalificaciones.py
@@ -0,0 +1,10 @@
+calificacion1 = int(input("Ingrese la Calificacion 1: "))
+calificacion2 = int(input("Ingrese la Calificacion 2: "))
+calificacion3 = int(input("Ingrese la Calificacion 3: "))
+suma = calificacion1 + calificacion2 + calificacion3
+promedio = suma / 3
+
+print(f"""Calificacion 1: {calificacion1} 
+      Calificación 2: {calificacion2}
+      Calificaciión 3: {calificacion3}
+      Promedio: {promedio: .0f}""")
